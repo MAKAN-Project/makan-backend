7f9b5a50f27e14527bde9a710b5c20bacd8194da
unknown <daliasaymah@gmail.com>
2025-10-04 19:56:45 +0300

    admin updates

--- airequests/admin.py
+++ airequests/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from .models import AIRequest
 
-# Register your models here.
-here here here
+@admin.register(AIRequest)
+class AIRequestAdmin(admin.ModelAdmin):
+    list_display = ('ai_request_id', 'user', 'style', 'created_at')
+    search_fields = ('user__first_name', 'user__last_name', 'style')
+    list_filter = ('style', 'created_at')
 
-alllll
