026309254580cf5f08335ba8c0d4cd8df46983c8
mhd <mahmoud.shuman@outlook.com>
2025-10-05 15:21:15 +0300

    the user dashboard forms

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,11 @@ urlpatterns = [
     path('login_user/', views.login_user, name="login_user"),
     path('logout_user/', views.logout_user, name='logout_user'),
     path('customer/dashboard/', views.customer_dashboard, name='customer_dashboard'),
+    path('reserve_session/', views.reserve_session, name='reserve_session'),
+    path('upload_room_photo/', views.upload_room_photo, name='upload_room_photo'),
+    path('create_building_project/', views.create_building_project, name='create_building_project'),
+    path('customer/files/', views.customer_files, name='customer_files'),
+    path('reschedule-session/<int:session_id>/', views.reschedule_session, name='reschedule_session'),
+    path('choose-building-stage/', views.choose_building_stage, name='choose_building_stage'),
+    path('engineering-fields/<str:stage>/', views.engineering_fields, name='engineering_fields'),
 ]
